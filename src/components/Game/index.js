d1183f1e890e3648db0362a6aa51d6e1c785082d
DTHutton <48737212+DTHutton@users.noreply.github.com>
2020-01-11 12:07:51 -0500

    Refactored a couple of things. Game now has win/loss function and resets on win/loss. Win/loss info is delivered via alerts which is kinda gross, but it works for now and that's what's important. Also, I added some extra comments to explain a couple lines because I completely forgot why I was writing some stuff they way I did.

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -4,28 +4,33 @@ import WordToGuess from './WordToGuess'
 import { wordbank } from './wordbank'
 import './index.css'
 
-export default function Game (props) {
-  // ? category
-  const [category] = useState(wordbank[Math.floor(Math.random() * wordbank.length)].title)
+export default function Game(props) {
+  // ? category selection
+  const initCategory = wordbank[Math.floor(Math.random() * wordbank.length)].title
+  const [category, setCategory] = useState(initCategory)
 
-  // ? word
-  function initWord () {
-    const num = wordbank.findIndex((index) => index.title === category)
-    const random = Math.floor(Math.random() * wordbank[num].items.length)
-    const selected = wordbank[num].items[random]
-    return selected
+  // ? word selection
+  function initWord() {
+    const categoryIndex = wordbank.findIndex((index) => index.title === category)
+    const random = Math.floor(Math.random() * wordbank[categoryIndex].items.length)
+    return wordbank[categoryIndex].items[random]
   }
-  const [word] = useState(initWord)
+  const [word, setWord] = useState(initWord)
 
-  // ? underscores
-  const [underscore, setUnderscore] = useState(word.replace(/./gi, '_').split(''))
+  // ? underscore generation
+  const initUnderscore = word.replace(/./gi, '_').split('')
+  const [underscore, setUnderscore] = useState(initUnderscore)
 
-  // ? keypress
+  // ? keypress validation and functions
   const [keyPressed, setKeyPressed] = useState()
   const keyValidate = /[a-z]|[0-9]/gi
-  const handleKeyPress = ({ key }) => (key.match(keyValidate) && key !== 'Enter'
-    ? setKeyPressed(key)
-    : alert(`${key} is not valid. Please select a valid alphanumeric key.`))
+  function handleKeyPress({ key }) {
+    if (key.match(keyValidate) && key !== 'Enter') {
+      setKeyPressed(key)
+    } else {
+      alert(`${key} is not valid. Please select a valid alphanumeric key.`)
+    }
+  }
   useEffect(() => {
     window.addEventListener('keypress', handleKeyPress)
     return () => {
@@ -34,37 +39,58 @@ export default function Game (props) {
   })
 
   // ? game score
+  const chances = 6
   const [incorrectGuess, setIncorrectGuess] = useState([])
-  const [remainingGuesses, setRemainingGuesses] = useState(6)
+  const [remainingGuesses, setRemainingGuesses] = useState(chances)
   // const [gameRegex, setGameRegex] = useState([])
   const [gameRegex] = useState([])
 
   useEffect(() => {
     if (word.includes(keyPressed)) {
-      //! this works, but i'm pretty sure it's sloppy. i've been looking into how to use state and effect hooks properly, but i don't understand why this works fine, but trying to write it like the setIncorrectGuess line below causes problems. it doesn't update the gameRegex state until you press another key what will update it again. i tried taking out the state hook and just using a variable set to an empty array, but it clears on re-render everytime even when the re-render is not related to the gameRegex data.
+      //! this works, but i'm pretty sure it's sloppy. i've been looking into how to use state and effect hooks properly, but i don't understand why this works fine, but trying to write it like the setIncorrectGuess line below causes problems. it doesn't update the gameRegex state until you press another key what will update it again. i tried taking out the state hook and just using a variable set to an empty array, but it clears on re-render.
       gameRegex.push(keyPressed)
       //! setGameRegex([...gameRegex, keyPressed])
       //! -----------------------------------------------------------------------------
+      //? converts the array to string and replaces ',' with '|' so it functions as the 'or' operator in RegEX
       const converter = gameRegex.join().replace(/,/gi, '|')
       const replacer = new RegExp(`[^${converter}]`, 'gi')
       const newUnderscore = word.replace(replacer, '_').split('')
       setUnderscore(newUnderscore)
-    } else {
+    }
+    if (!word.includes(keyPressed) && !incorrectGuess.includes(keyPressed)) {
       // const newIncorrectGuess = [...incorrectGuess, keyPressed]
       // setIncorrectGuess(newIncorrectGuess)
       setIncorrectGuess([...incorrectGuess, keyPressed])
-      setRemainingGuesses(6 - incorrectGuess.length)
+      setRemainingGuesses(chances - incorrectGuess.length)
     }
     //! this warning is returned by React when the app reneders. need to research it because adding the missing dependencies causes an infinite render loop.
     /*
-
     ./src/components/Game/index.js
     Line 58:6:  React Hook useEffect has missing dependencies: 'gameRegex', 'incorrectGuess', and 'word'.
     Either include them or remove the dependency array. You can also do a functional update 'setIncorrectGuess(i => ...)'
     if you only need 'incorrectGuess' in the 'setIncorrectGuess' call  react-hooks/exhaustive-deps
-
     */
   }, [keyPressed])
+
+  //? game win
+  useEffect(() => {
+    if (!underscore.includes('_')) {
+      alert('Winner!!!')
+      setCategory(initCategory)
+      setWord(initWord)
+      setUnderscore(initUnderscore)
+      setIncorrectGuess([])
+      setRemainingGuesses(chances)
+    }
+    if (!remainingGuesses) {
+      alert(`Better luck next time! The word was: ${word}.`)
+      setCategory(initCategory)
+      setWord(initWord)
+      setUnderscore(initUnderscore)
+      setIncorrectGuess([])
+      setRemainingGuesses(chances)
+    }
+  })
 
   return (
     <div className="container--game">
